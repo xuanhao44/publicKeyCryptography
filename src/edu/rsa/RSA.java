c30538903a55a5315fb5be97f5858bc21cff0c5d
Xuanhao <shirou-emiya2002@outlook.com>
2022-10-13 17:05:24 +0800

    rsa init

---
+++ src/edu/rsa/RSA.java
@@ -0,0 +1,194 @@
+package edu.rsa;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RSA {
+
+    /**
+     * 读入明文文件
+     * <p>
+     * 编码方式: 每次读入两个 byte, 如 a, b
+     * ascii 码为 97 98
+     * 将其组合为 097098 的 最高为 6 位的十进制数
+     * 转化为 BigInteger, 存入数组
+     * 如果一次没读满两个 byte, 用 000 补足
+     *
+     * @param inName 明文文件路径
+     * @return mByteList 编码后的明文编码序列
+     */
+    private static List<BigInteger> readPlain(String inName) throws IOException {
+        FileInputStream fis = new FileInputStream(inName);
+
+        int by, byPre;
+        int k1 = 1_000;
+        BigInteger temp;
+        List<BigInteger> mByteList = new ArrayList<>();
+
+        do {
+            // 这里只能这么读
+            by = fis.read();
+            byPre = fis.read();
+
+            if ((by != -1) && (byPre != -1)) {
+                // 正常两个
+                temp = BigInteger.valueOf(by * k1 + byPre);
+                mByteList.add(temp);
+            } else if (by != -1) {
+                // 只有一个, 由于 else if 就不需要判断 byPre
+                temp = BigInteger.valueOf(by * k1);
+                mByteList.add(temp);
+                break;
+            } else {
+                break;
+            }
+        } while (true);
+
+        // 释放系统资源
+        fis.close();
+
+        return mByteList;
+    }
+
+    /**
+     * 写入密文文件
+     * <p>
+     * 密文分组长度 <= nLength, 且长度不一
+     * 全部补足至 nLength 长度, 填入文件
+     * 对于长度不足的情况, 要前面补 0
+     *
+     * @param cByteList 编码后的明文编码序列
+     * @param nLength   密文分组长度
+     * @param deName    明文文件路径
+     */
+    private static void writeCrypto(List<BigInteger> cByteList, int nLength, String deName) throws IOException {
+        FileOutputStream fos = new FileOutputStream(deName);
+
+        for (BigInteger byte2 : cByteList) {
+            int len = byte2.toString().length(); // 需要的是字节数
+            String str = "0".repeat(Math.max(0, nLength - len)) + byte2; // 前面补 0
+            fos.write(str.getBytes());
+        }
+
+        // 释放系统资源
+        fos.close();
+    }
+
+    /**
+     * 加密过程
+     *
+     * @param rsaUtils RSA 密钥
+     * @param in       明文文件路径
+     * @param en       密文文件路径
+     */
+    private static void enCrypt(RSAUtils rsaUtils, String in, String en) throws IOException {
+        // 读入明文, 得到序列
+        List<BigInteger> mByteList = readPlain(in);
+
+        // 对分组逐个加密
+        List<BigInteger> cByteList = new ArrayList<>();
+        for (BigInteger byte2 : mByteList)
+            cByteList.add(rsaUtils.en(byte2));
+
+        // 写入密文文件
+        writeCrypto(cByteList, rsaUtils.getN().bitLength(), en);
+    }
+
+
+    /**
+     * 读入密文文件
+     * <p>
+     * 读入的密文文件的字节长度是标准的 nLength 的倍数
+     * 一次读入 nLength 长度的字节
+     *
+     * @param nLength 密文分组长度
+     * @param enName  密文文件路径
+     * @return cByteList 密文编码序列
+     */
+    private static List<BigInteger> readCrypto(int nLength, String enName) throws IOException {
+        FileInputStream fis = new FileInputStream(enName);
+
+        List<BigInteger> cByteList = new ArrayList<>();
+
+        // 一次读入 nLength 长度的 byte
+        byte[] tempData = new byte[nLength];
+        while (fis.read(tempData) != -1)
+            // 这种 BigInteger 构造不允许有空格等 blanks, 此处可以满足
+            // 利用 String 去掉前导 0
+            cByteList.add(new BigInteger(new String(tempData)));
+
+        // 释放系统资源
+        fis.close();
+
+        return cByteList;
+    }
+
+    /**
+     * 写入解密文件
+     * <p>
+     * 按照之前读入的编码规则解码
+     *
+     * @param dByteList 解码得到的明文编码序列
+     * @param deName    解码明文文件路径
+     */
+    private static void writePlain(List<BigInteger> dByteList, String deName) throws IOException {
+        FileOutputStream fos = new FileOutputStream(deName);
+
+        BigInteger by, byNext;
+        BigInteger k1 = BigInteger.valueOf(1000);
+        for (BigInteger byte2 : dByteList) {
+            by = byte2.divide(k1);
+            byNext = byte2.mod(k1);
+            fos.write(by.intValue());
+            if (byNext.intValue() != 0) {
+                // 把 0 加进去会有奇怪的字符
+                fos.write(byNext.intValue());
+            }
+        }
+
+        // 释放系统资源
+        fos.close();
+    }
+
+    /**
+     * 解密过程
+     *
+     * @param rsaUtils RSA 密钥
+     * @param en       密文文件路径
+     * @param de       解密文件路径
+     */
+    private static void deCrypt(RSAUtils rsaUtils, String en, String de) throws IOException {
+        // 按照密文分组长度读入密文
+        List<BigInteger> cByteList = readCrypto(rsaUtils.getN().bitLength(), en);
+
+        // 对分组逐个解密
+        List<BigInteger> dByteList = new ArrayList<>();
+        for (BigInteger byte2 : cByteList)
+            dByteList.add(rsaUtils.de(byte2));
+
+        // 写入解密文件
+        writePlain(dByteList, de);
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        String pu = "keys/publicKey";
+        String pr = "keys/privateKey";
+
+        String in = "inData/lab2-Plaintext.txt";
+        String en = "outData/lab2-Encrypt-text.txt";
+        String de = "outData/lab2-Decrypt-text.txt";
+
+        int bitLength = 10;
+        RSAUtils rsaUtils = new RSAUtils(bitLength);
+        rsaUtils.genKeys(pu, pr);
+        rsaUtils.printAll();
+
+        enCrypt(rsaUtils, in, en);
+        deCrypt(rsaUtils, en, de);
+    }
+}
---
+++ src/edu/rsa/RSAUtils.java
@@ -0,0 +1,131 @@
+package edu.rsa;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.math.BigInteger;
+import java.util.Random;
+
+public class RSAUtils {
+
+    /**
+     * 大素数 p
+     */
+    private final BigInteger p;
+    /**
+     * 大素数 q
+     */
+    private final BigInteger q;
+    /**
+     * 大素数乘积 n
+     */
+    private final BigInteger n;
+    /**
+     * n 的欧拉函数值
+     */
+    private final BigInteger phiN;
+    /**
+     * 指数 e
+     */
+    private final BigInteger e;
+    /**
+     * 指数逆元 d
+     */
+    private final BigInteger d;
+
+    /**
+     * 生成密钥
+     *
+     * @param bitLength 大素数长度
+     */
+    public RSAUtils(int bitLength) {
+        Random r = new Random();
+
+        p = BigInteger.probablePrime(bitLength, r);
+        q = p.nextProbablePrime();
+        n = p.multiply(q);
+        // phiN = (p - 1) * (q - 1)
+        phiN = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+        // gcd(e, phiN) = 1, 且 e 不可以太小, 同时 d 也不可以太小, 于是随机生成一个 bitLength 长度的 e
+        BigInteger temp = new BigInteger(bitLength, r);
+        while (!temp.gcd(phiN).equals(BigInteger.ONE)) {
+            temp = temp.add(BigInteger.ONE);
+        }
+        e = temp;
+        // d = e^{-1} mod phiN
+        d = e.modInverse(phiN);
+    }
+
+    /**
+     * 对明文加密 m^e mod n
+     *
+     * @param m 明文
+     * @return m.modPow(e, n) 密文
+     */
+    public BigInteger en(BigInteger m) {
+        return m.modPow(e, n);
+    }
+
+    /**
+     * 对密文解密 c^d mod n
+     *
+     * @param c 密文
+     * @return c.modPow(d, n) 解密明文
+     */
+    public BigInteger de(BigInteger c) {
+        return c.modPow(d, n);
+    }
+
+    /**
+     * 输出参数
+     */
+    public void printAll() {
+        System.out.println("p = " + p + ", len(p) = " + p.bitLength());
+        System.out.println("q = " + q + ", len(q) = " + q.bitLength());
+        System.out.println("n = " + n + ", len(n) = " + n.bitLength());
+        System.out.println("phiN  = " + phiN + ", len(phiN) = " + phiN.bitLength());
+        System.out.println("e  = " + e + ", len(e) = " + e.bitLength());
+        System.out.println("gcd(e, phiN) = " + e.gcd(phiN));
+        System.out.println("d  = " + d + ", len(d) = " + d.bitLength());
+        System.out.println("e * d mod phiN = " + e.multiply(d).mod(phiN));
+    }
+
+    /**
+     * 获得公钥 {e, n}
+     *
+     * @return publicKey
+     */
+    private String getPublicKey() {
+        return e.toString() + n.toString();
+    }
+
+    /**
+     * 获得秘钥 {d, n}
+     *
+     * @return privateKey
+     */
+    private String getPrivateKey() {
+        return d.toString() + n.toString();
+    }
+
+    /**
+     * 输出公钥对
+     *
+     * @param puName 公钥文件路径
+     * @param prName 密钥文件路径
+     */
+    public void genKeys(String puName, String prName) throws IOException {
+        FileOutputStream fosPu = new FileOutputStream(puName);
+        FileOutputStream fosPr = new FileOutputStream(prName);
+
+        fosPu.write(getPublicKey().getBytes());
+        fosPr.write(getPrivateKey().getBytes());
+
+        fosPu.close();
+        fosPr.close();
+
+    }
+
+    public BigInteger getN() {
+        return n;
+    }
+}
